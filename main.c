e05be9f9f3bebeed4fbfd07e6dc8e37914bdd57f
Ade2002 <sikirusulaimon2002@gmail.com>
2022-05-14 22:09:45 +0100

    ...

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("1\n");
+    printf("4 6\n");
+    printf("9 11 13\n");
+    printf("17 19 21 23\n");
+    /* printf("1\n");
+    printf("1\n");
+    printf("1\n");
+    printf("1\n");
+    printf("1\n");
+    printf("1\n");
+    printf("1\n");
+    printf("1\n");
+    printf("1\n"); */
+    return 0;
+}
