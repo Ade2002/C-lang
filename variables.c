4146fac54a366661415dfd5b336b1038441f5859
Ade2002 <sikirusulaimon2002@gmail.com>
2022-05-19 19:27:21 +0100

    c variables

---
+++ variables.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hi my name is Ade\n");
+    printf("I am a young adult,\n");
+    printf("I really love coding\n");
+    printf("but I thinks C is very difficult.\n");
+
+    return 0;
+}
