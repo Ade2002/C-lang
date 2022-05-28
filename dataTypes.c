a494e35b6328565ae38bc3770c29cbe658a71666
Ade2002 <sikirusulaimon2002@gmail.com>
2022-05-28 03:33:22 +0100

    c dataTypes

--- dataTypes.c
+++ dataTypes.c
@@ -1,2 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+int	main(int argc, char **argv)
+{
+    int number = 20;
+    double decimalNumbers = 3.0;
+    char letter = 'A';
+    char stringArray[] = "Ade is cool";
+    system("pause");
+    return 0;
+}
