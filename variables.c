d61a8644bc9ff2f94ca8adb98b3e60d7dd97f7d6
Ade2002 <sikirusulaimon2002@gmail.com>
2022-05-28 03:11:41 +0100

    c variables

--- variables.c
+++ variables.c
@@ -4,9 +4,9 @@
 int main()
 {
     char characterName[] = "Ade";
-    ch
-    printf("Hi my name is Ade\n");
-    printf("I am a young adult,\n");
+    int adeAge = 20;
+    printf("Hi my name is %s\n", characterName);
+    printf("I am a young adult, %d\n", adeAge);
     printf("I really love coding\n");
     printf("but I thinks C is very difficult.\n");
 
