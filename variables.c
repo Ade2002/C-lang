17383f1e3f9885b98411db6fe2c291f577eb0d68
Ade2002 <sikirusulaimon2002@gmail.com>
2022-05-28 03:19:28 +0100

    c variables

--- variables.c
+++ variables.c
@@ -7,8 +7,8 @@ int main()
     int adeAge = 20;
     printf("Hi my name is %s\n", characterName);
     printf("I am a young adult, %d\n", adeAge);
-    printf("I really love coding\n");
-    printf("but I thinks C is very difficult.\n");
+    printf("%s really love coding\n", characterName);
+    printf("but %s think C is very difficult.\n", characterName);
 
     return 0;
 }
