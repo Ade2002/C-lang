2ae9a841ce4bb6e87dcfde689c0992066f7bdb76
Ade2002 <sikirusulaimon2002@gmail.com>
2022-05-29 11:20:45 +0100

    input

--- input.c
+++ input.c
@@ -4,13 +4,13 @@
 int main(int argc, char const *argv[])
 {
 
-    int age;
-    int Name;
+    int Name;/* 
     printf("Hi there, whats your age?:");
+    int age;
     scanf("%d", &age);
-    printf("yay!! your age is %d\n", age);
+    printf("yay!! your age is %d\n", age); */
     printf("Hi there, whats your name?\n");
     scanf("%c", &Name);
-    printf("yay!! your name is %s and your age is %d\n", Name, age);
+    printf("yay!! your name is %s and your age is %d\n", Name /* age */);
     return 0;
 }
