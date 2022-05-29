056b2204332c4d08d0923fec6164395e7e037954
Ade2002 <sikirusulaimon2002@gmail.com>
2022-05-29 09:43:03 +0100

    printf

--- printf.c
+++ printf.c
@@ -7,7 +7,7 @@ int	main(int argc, char **argv)
     printf("Ade age is %f \n", 20.1);
     printf("Ade %s is %f \n", "age", 20.1);
     printf("%f \n", 5.4 + 5.8);
-    printf("%d \n", pow(2,3));
+    printf("%f \n", pow(2,3));
     system("pause");
     return 0;
 }
