2d71dcd9170574c3736507c771474a4cb5a6d55b
Ade2002 <sikirusulaimon2002@gmail.com>
2022-05-29 11:44:31 +0100

    input

--- input.c
+++ input.c
@@ -9,7 +9,7 @@ int main(int argc, char const *argv[])
     printf("Hi there, whats your name?\n");
     scanf("%s", &Name);
     printf("welcome %s\n", Name);
-    printf("are you eligible to vote if yes then whats your age?:");
+    printf("are you eligible to vote?\", if yes then whats your age?:");
     scanf("%d", &age);
     printf("yay!! your name is %s and your age is %d now you can cast your vote\n", Name, age);
     system("pause");
