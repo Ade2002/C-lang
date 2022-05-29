945651c776040f65cddb2a7607c2e090813adc79
Ade2002 <sikirusulaimon2002@gmail.com>
2022-05-29 11:19:45 +0100

    input

--- input.c
+++ input.c
@@ -9,9 +9,8 @@ int main(int argc, char const *argv[])
     printf("Hi there, whats your age?:");
     scanf("%d", &age);
     printf("yay!! your age is %d\n", age);
-    system("pause");
     printf("Hi there, whats your name?\n");
     scanf("%c", &Name);
-    printf("yay!! your name is %s\n", Name);
+    printf("yay!! your name is %s and your age is %d\n", Name, age);
     return 0;
 }
