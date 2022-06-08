6b5ab199ecce6e8f355f6cee6aa989aeaff71672
Ade2002 <sikirusulaimon2002@gmail.com>
2022-06-08 20:59:47 +0100

    ...

--- input.c
+++ input.c
@@ -12,9 +12,9 @@ int main(int argc, char const *argv[])
     printf("welcome %s\n", Name);
     printf("are you eligible to vote?\", if yes then whats your last name and other names?:");
     fgets(lastName, 20, stdin);
-    printf("Hi %s\n", strcat(Name, lastName));
+   /*  printf("Hi %s\n", strcat(Name, lastName));
     scanf("%d", &age);
     printf("yay!! your name is %s and your age is %d now you can cast your vote\n", Name, age);
-    system("pause");
+    system("pause"); */
     return 0;
 }
